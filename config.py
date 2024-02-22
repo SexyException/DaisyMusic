ec07aecb87d81cd374bc89d4eb7c8ce6b2cf3689
EXCEPTION <142222853+SexyException@users.noreply.github.com>
2024-02-22 20:09:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import re
-import os
 from os import getenv
+
 from dotenv import load_dotenv
 from pyrogram import filters
 
@@ -9,19 +9,20 @@ load_dotenv()
 # Get this value from my.telegram.org/apps
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
+
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 16000))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 360))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID"))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID","-1002044950428"))
-# Get this value from  on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID"))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002051250529))
+
+# Get this value from @SanjanaXRobot on Telegram by /id
+OWNER_ID = int(getenv("OWNER_ID", 7050009951))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -38,30 +39,20 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PromotersShehzada")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/class_11th_quiz_groups_2024")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TheDaisyUpdates")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TheDaisySupport")
 
-# Maximum Limit Allowed for users to save playlists on bot's server
-SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "3000"))
-
-# MaximuM limit for fetching playlist's track from youtube, spotify, apple links.
-PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", "2500"))
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = False
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 
-#Auto Gcast/Broadcast Handler (True = broadcast on , False = broadcast off During Hosting, Dont Do anything here.)
-AUTO_GCAST = os.getenv("AUTO_GCAST")
-
-#Auto Broadcast Message That You Want Use In Auto Broadcast In All Groups.
-AUTO_GCAST_MSG = getenv("AUTO_GCAST_MSG", "")
 
 # Get this credentials from https://developer.spotify.com/dashboard
-SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "19609edb1b9f4ed7be0c8c1342039362")
-SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "409e31d3ddd64af08cfcc3b0f064fcbe")
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
+SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
 
 
 # Maximum limit for fetching playlist's track from youtube, spotify, apple links.
-PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 2500))
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
 
 
 # Telegram audio and video file size limit (in bytes)
@@ -69,27 +60,13 @@ TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 # Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
 
-# Time after which bot will suggest random chats about bot commands.
-AUTO_SUGGESTION_TIME = int(
-    getenv("AUTO_SUGGESTION_TIME", "3")
-)  # Remember to give value in Seconds
 
-# Set it True if you want to bot to suggest about bot commands to random chats of your bots.
-AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", "True")
-# Cleanmode time after which bot will delete its old messages from chats
-CLEANMODE_DELETE_MINS = int(
-    getenv("CLEANMODE_MINS", "5")
-)  # Remember to give value in Seconds
-
-# Get your pyrogram v2 session from @MOON_STRING_ROBOT on Telegram
+# Get your pyrogram v2 session from @StringFatherBot on Telegram
 STRING1 = getenv("STRING_SESSION", None)
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
 STRING5 = getenv("STRING_SESSION5", None)
-#
-                                                                                      
-                                                                                       
 
 
 BANNED_USERS = filters.user()
@@ -98,33 +75,29 @@ lyrical = {}
 votemode = {}
 autoclean = []
 confirmer = {}
-chatstats = {}
-userstats = {}
-clean = {}
 
-autoclean = []
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org/file/d5d74065ef9e2516ce4e2.jpg"
+    "START_IMG_URL", "https://graph.org/file/d10b5370e7b51555b47d6.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org/file/75d9cbc6955c884deb021.jpg"
+    "PING_IMG_URL", "https://graph.org/file/b868da20f5a9ca72f2c35.jpg"
 )
-PLAYLIST_IMG_URL = "https://telegra.ph/file/aa015718299510040d41f.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/aa015718299510040d41f.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/aa015718299510040d41f.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/aa015718299510040d41f.jpg"
-STREAM_IMG_URL = "https://telegra.ph/file/aa015718299510040d41f.jpg"
-SOUNCLOUD_IMG_URL = "https://telegra.ph/file/aa015718299510040d41f.jpg"
-YOUTUBE_IMG_URL = "https://telegra.ph/file/aa015718299510040d41f.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/aa015718299510040d41f.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/aa015718299510040d41f.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/aa015718299510040d41f.jpg"
+PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
+STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
+TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
+SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
+YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
 
 
 def time_to_seconds(time):
     stringt = str(time)
-    return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
+    return sum(int(x) * 360**i for i, x in enumerate(reversed(stringt.split(":"))))
 
 
 DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
