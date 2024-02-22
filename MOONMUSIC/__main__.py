821f87d8ac06036b2322e209eb1101e747e387a0
EXCEPTION <142222853+SexyException@users.noreply.github.com>
2024-02-23 01:10:54 +0530

    Update __main__.py

--- MOONMUSIC/__main__.py
+++ MOONMUSIC/__main__.py
@@ -36,27 +36,18 @@ async def init():
     await app.start()
     for all_module in ALL_MODULES:
         importlib.import_module("MOONMUSIC.plugins" + all_module)
-    LOGGER("MOONMUSIC.plugins").info("All Loaded Get Ready To Fyt...⚡️")
+    LOGGER("MOONMUSIC.plugins").info("ALL ARE LOADED...")
     await userbot.start()
     await MOON.start()
-try:
-        await MOON.stream_call("https://graph.org/file/228c36d54ca891d0310f5.mp4")
-    except NoActiveGroupCall:
-        LOGGER("MOONMUSIC.plugins").error(
-            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
-        )
-        exit()
-    except:
-        pass
     await MOON.decorators()
     LOGGER("MOONMUSIC").info(
-        "\x41\x6e\x6f\x6e\x58\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\n\n\x44\x6f\x6e'\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x46\x61\x6c\x6c\x65\x6e\x41\x73\x73\x6f\x63\x69\x61\x74\x69\x6f\x6e"
+        "╔═════ஜ۩۞۩ஜ════╗\n  ᴍᴀᴅᴇᴇ ʙʏ ᴇxᴄᴇᴘᴛɪᴏɴ ♨\n╚═════ஜ۩۞۩ஜ════╝"
     )
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("MOONMUSIC").info("Stopping DAISY MusicBot...")
+    LOGGER("MOONMUSIC").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨ᴍᴀᴅᴇ ʙᴛ ᴇxᴄᴇᴘᴛɪᴏɴ\n╚═════ஜ۩۞۩ஜ════╝")
+    
 
-
-if name == "main":
+if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(init())
